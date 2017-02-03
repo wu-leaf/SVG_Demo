e740a0e0de2d45ae39c6034fbd0df184c660fa9a
wu-leaf <375790798@qq.com>
2017-02-03 16:48:39 +0800

    the first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# SVG_Demo
+A simple demo of SVG
+
+# Libraries
+>com.jrummyapps:animated-svg-view:1.0.1
+
+>com.android.support:animated-vector-drawable:${latest_version}
+
+
+## Screenshots
+<img src="screenshots/GIF.gif" width="300">
+<img src="screenshots/GIF2.gif" width="300">
+
+
+
+
+
+
+
+
+
+
+
+##About me
+I am a student in China, I love reading novel, love music. 
+At the same time, I am also obsessed with writing code.
+If you have any new idea about this project,welcome to [contact me:veyron_gz@163.com](mailto:veyron_gz@163.com). :smiley:
+
+In addition, my blog: [http://blog.csdn.net/leaf_130](http://blog.csdn.net/leaf_130)
+
+
+
+
+
---
+++ app/src/main/java/com/veyron/www/svg_demo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.veyron.www.svg_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+
+import com.jrummyapps.android.widget.AnimatedSvgView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimatedSvgView svgView;
+    private int index = -1;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        svgView = (AnimatedSvgView) findViewById(R.id.animated_svg_view);
+
+        svgView.postDelayed(new Runnable() {
+
+            @Override public void run() {
+                svgView.start();
+            }
+        }, 500);
+
+        svgView.setOnClickListener(new View.OnClickListener() {
+
+            @Override public void onClick(View v) {
+                if (svgView.getState() == AnimatedSvgView.STATE_FINISHED) {
+                    svgView.start();
+                }
+            }
+        });
+
+        svgView.setOnStateChangeListener(new AnimatedSvgView.OnStateChangeListener() {
+
+            @Override public void onStateChange(int state) {
+                if (state == AnimatedSvgView.STATE_TRACE_STARTED) {
+                    findViewById(R.id.btn_previous).setEnabled(false);
+                    findViewById(R.id.btn_next).setEnabled(false);
+                } else if (state == AnimatedSvgView.STATE_FINISHED) {
+                    findViewById(R.id.btn_previous).setEnabled(index != -1);
+                    findViewById(R.id.btn_next).setEnabled(true);
+                    if (index == -1) index = 0; // first time
+                }
+            }
+        });
+    }
+
+    public void onNext(View view) {
+        if (++index >= SVG.values().length) index = 0;
+        setSvg(SVG.values()[index]);
+    }
+
+    public void onPrevious(View view) {
+        if (--index < 0) index = SVG.values().length - 1;
+        setSvg(SVG.values()[index]);
+    }
+
+    private void setSvg(SVG svg) {
+        svgView.setGlyphStrings(svg.glyphs);
+        svgView.setFillColors(svg.colors);
+        svgView.setViewportSize(svg.width, svg.height);
+        svgView.setTraceResidueColor(0x32000000);
+        svgView.setTraceColors(svg.colors);
+        svgView.rebuildGlyphData();
+        svgView.start();
+    }
+
+}
